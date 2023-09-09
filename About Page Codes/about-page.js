16f0302bc9636aba34bb796c2c7484fcbb7ce933
Vishnu Kumar Verma <125765877+VishnuKumarVerma@users.noreply.github.com>
2023-09-09 17:21:20 +0000

    footer codes with proper assigning

--- About Page Codes/about-page.js
+++ About Page Codes/about-page.js
@@ -4,32 +4,6 @@ window.onload = function () {
   document.getElementsByTagName('input').value = '';
 }
 
-// javascript code to minimize the nav-bar on scroll
-
-window.onscroll = function () { scrollFunction() };
-
-function scrollFunction() {
-  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
-    docemument.getElementById("header-nav").style.height = "3.4r";
-    document.getElementById("logo").style.height = "6rem";
-
-  } else {
-    document.getElementById("header-nav").style.height = "3.8rem";
-    document.getElementById("logo").style.height = "10rem";
-
-  }
-}
-
-// javascript for opening and closing of hamburger menu
-
-let menu = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-
-menu.onclick = () => {
-  menu.classList.toggle('bx-x');
-  navbar.classList.toggle('open');
-}
-
 // javascript for contact with us button
 
 function contactButton() {
