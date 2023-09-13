056849e2af343525eb67e7a982205a57628a8972
Vishnu Kumar Verma <125765877+VishnuKumarVerma@users.noreply.github.com>
2023-09-13 04:49:40 +0000

    make text animation more accurate...

--- About Page Codes/about-page.js
+++ About Page Codes/about-page.js
@@ -30,6 +30,23 @@ menu.onclick = () => {
   navbar.classList.toggle('open');
 }
 
+// javascript for the typing animation 
+
+let title = document.querySelector('.main_heading');
+let index = 1;
+
+const typeWriter = () => {
+    let name = "Faster-Ontime-Reliable";
+    let new_title = name.slice(0,index);
+    title.innerText = new_title;
+
+    index > name.length ? index = 1 : index++;
+
+    setTimeout(() => typeWriter(), 100)
+}
+
+typeWriter();
+
 // javascript for contact with us button
 
 function contactButton() {
