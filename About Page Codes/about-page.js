898c1304134e50b481ee56eb17d22db4ecb0abde
Vishnu Kumar Verma <125765877+VishnuKumarVerma@users.noreply.github.com>
2023-09-17 10:11:47 +0000

    changes all the things that i had called...

--- About Page Codes/about-page.js
+++ About Page Codes/about-page.js
@@ -1,34 +1,32 @@
-// javascript code to refresh the input field when reload or cut
-
-window.onload = function () {
-  document.getElementsByTagName('input').value = '';
-}
-
-// javascript code to minimize the nav-bar on scroll
-
-window.onscroll = function () { scrollFunction() };
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
-    docemument.getElementById("header-nav").style.height = "3.4r";
-    document.getElementById("logo").style.height = "6rem";
-
+  if (
+      document.body.scrollTop > 40 ||
+      document.documentElement.scrollTop > 40
+  ) {
+      document.getElementById("navbar").style.height = "3.4vw";
+      document.getElementById("logo1").style.height = "3.8vw";
   } else {
-    document.getElementById("header-nav").style.height = "3.8rem";
-    document.getElementById("logo").style.height = "10rem";
-
+      document.getElementById("navbar").style.height = "5vw";
+      document.getElementById("logo1").style.height = "4.5vw";
   }
 }
 
-// javascript for opening and closing of hamburger menu
+// to make ham menu responsive
 
-let menu = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+const toggleBtn = document.querySelector(".toggle_btn");
+const toggleBtnIcon = document.querySelector(".toggle_btn i");
+const dropDownMenu = document.querySelector(".dropdown_menu");
 
-menu.onclick = () => {
-  menu.classList.toggle('bx-x');
-  navbar.classList.toggle('open');
-}
+toggleBtn.addEventListener("click", () => {
+  dropDownMenu.classList.toggle("open");
+  const isOpen = dropDownMenu.classList.contains("open");
+
+  toggleBtnIcon.classList = isOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars";
+});
 
 // javascript for the typing animation 
 
@@ -46,6 +44,22 @@ const typeWriter = () => {
 }
 
 typeWriter();
+
+// javascript for the who we are typing animation
+
+let text = document.querySelector('.who-we-are-text');
+let new_index = 1;
+
+const typeWriternew = () => {
+    let name = "As a dedicated team of individuals who deeply care about your precious time and the well-being of your loved ones, we intimately understand the distress that ensues from the untimely response of hospitals and ambulances during critical junctures. Our mission revolves around reshaping the perception of medical facilities in India, replacing fear with assurance. We aim to imbue our fellow citizens with a renewed sense of security, trusting in a healthcare system that values both time and lives.";
+    let new_text = name.slice(0,new_index);
+    text.innerText = new_text;
+   new_index++;
+
+    setTimeout(() => typeWriternew(), 20)
+}
+
+typeWriternew();
 
 // javascript for contact with us button
 
@@ -73,3 +87,43 @@ function submitfunc() {
 function browsefunc() {
   document.getElementById("thanks-popup").style.display = "none";
 }
+
+                        // meet or team swipper slides
+                        
+var swiper = new Swiper(".swiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 3,
+    slideShadows: true
+  },
+  keyboard: {
+    enabled: true
+  },
+  mousewheel: {
+    thresholdDelta: 70
+  },
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2
+    },
+    768: {
+      slidesPerView: 1
+    },
+    1024: {
+      slidesPerView: 2
+    },
+    1560: {
+      slidesPerView: 3
+    }
+  }
+});     
